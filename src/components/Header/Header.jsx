2a2bf3dce5fc09f203a787088cdf99adf4cdeff3
developermickey <mukesh.quazma@gmail.com>
2024-01-09 13:26:03 +0530

    Header fixed large screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 function Header() {
   return (
-    <div className="w-full h-[130px] bg-zinc-400 text-white">
+    <div className="w-full h-[130px] bg-zinc-400 text-white ">
       <div className="topbar w-full h-10 sm:h-12 bg-[#9ea18e] text-center flex items-center justify-center ">
         <p className="sm:text-xs">
           We ship nationwide. 30-day return policy. Free standard shipping on
           orders over $75.
         </p>
       </div>
-      <div className="mainheader w-full h-24 bg-white text-black flex items-center justify-between px-12">
+      <div className="mainheader w-full h-24 bg-white text-black flex items-center justify-between px-24 headers">
         <img className="w-40 sm:w-24" src="./logo.jpg" alt="Logo" />
         <ul className="flex items-center justify-between gap-8 sm:gap-5">
           <li>
