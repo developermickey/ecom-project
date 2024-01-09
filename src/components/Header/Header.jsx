d9a38a12b9254dca3a45d1810c14eeddff37b04f
developermickey <mukesh.quazma@gmail.com>
2024-01-09 13:08:45 +0530

    code added mobile respoinve don header and footer hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,42 @@
 function Header() {
   return (
     <div className="w-full h-[130px] bg-zinc-400 text-white">
-      <div className="topbar w-full h-10 bg-[#9ea18e] text-center flex items-center justify-center">
-        <p>
+      <div className="topbar w-full h-10 sm:h-12 bg-[#9ea18e] text-center flex items-center justify-center ">
+        <p className="sm:text-xs">
           We ship nationwide. 30-day return policy. Free standard shipping on
           orders over $75.
         </p>
       </div>
       <div className="mainheader w-full h-24 bg-white text-black flex items-center justify-between px-12">
-        <img className="w-40" src="./logo.jpg" alt="Logo" />
-        <ul className="flex items-center justify-between gap-8">
+        <img className="w-40 sm:w-24" src="./logo.jpg" alt="Logo" />
+        <ul className="flex items-center justify-between gap-8 sm:gap-5">
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600 sm:text-sm" href="">
               Home
             </a>
           </li>
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600 sm:text-sm" href="">
               About Us
             </a>
           </li>
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600 sm:text-sm" href="">
               Shop
             </a>
           </li>
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600 sm:text-sm" href="">
               All Products
             </a>
           </li>
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600sm:text-sm" href="">
               Blog
             </a>
           </li>
           <li>
-            <a className="hover:text-amber-600" href="">
+            <a className="hover:text-amber-600 sm:text-sm" href="">
               Contact Us
             </a>
           </li>
