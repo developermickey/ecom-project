efd5766b49998ac4b3ee9d34fc99b5db09b7dcaf
developermickey <mukesh.quazma@gmail.com>
2024-01-09 16:39:43 +0530

    Code added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -9,11 +9,11 @@ function HeroSection() {
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
           <div className="bg-[url(./assets/1.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="lg:pl-24 sm:pl-0 sm:text-center">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
               <p className="text-lg text-white pb-5 hero-p">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-white text-left w-full pb-8 hero-heading">
+              <h1 className="lg:text-6xl sm:text-2xl text-white text-left w-full pb-8 hero-heading">
                 Enhance Your
                 <br />
                 Natural Beauty
@@ -26,11 +26,11 @@ function HeroSection() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url(./assets/2.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="pl-24">
-              <p className="text-lg text-black pb-5">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
+              <p className="text-lg text-black pb-5 hero-p-black">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-black text-left w-full pb-8">
+              <h1 className="lg:text-6xl sm:text-2xl text-black text-left w-full pb-8 hero-heading-black">
                 Enhance Your
                 <br />
                 Natural Beauty
@@ -43,11 +43,11 @@ function HeroSection() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url(./assets/3.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="pl-24">
-              <p className="text-lg text-white pb-5">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
+              <p className="text-lg text-white pb-5 hero-p">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-white text-left w-full pb-8">
+              <h1 className="lg:text-6xl sm:text-2xl text-white text-left w-full pb-8 hero-heading">
                 Enhance Your
                 <br />
                 Natural Beauty
@@ -60,11 +60,11 @@ function HeroSection() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url(./assets/1.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="pl-24">
-              <p className="text-lg text-white pb-5">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
+              <p className="text-lg text-white pb-5 hero-p">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-white text-left w-full pb-8">
+              <h1 className="lg:text-6xl sm:text-2xl text-white text-left w-full pb-8 hero-heading">
                 Enhance Your
                 <br />
                 Natural Beauty
@@ -77,11 +77,11 @@ function HeroSection() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url(./assets/2.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="pl-24">
-              <p className="text-lg text-black pb-5">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
+              <p className="text-lg text-black pb-5 hero-p-black">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-black text-left w-full pb-8">
+              <h1 className="lg:text-6xl sm:text-2xl text-black text-left w-full pb-8 hero-heading-black">
                 Enhance Your
                 <br />
                 Natural Beauty
@@ -94,11 +94,11 @@ function HeroSection() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url(./assets/3.jpg)] w-full h-screen lg:h-screen sm:h-[50vh] bg-cover bg-no-repeat bg-center flex items-center justify-start text-left">
-            <div className="pl-24">
-              <p className="text-lg text-white pb-5">
+            <div className="lg:pl-24 sm:pl-0 sm:text-center sideSec">
+              <p className="text-lg text-white pb-5 hero-p">
                 CLEAN BEAUTY • CRUELTY FREE
               </p>
-              <h1 className="text-6xl text-white text-left w-full pb-8">
+              <h1 className="lg:text-6xl sm:text-2xl text-white text-left w-full pb-8 hero-heading">
                 Enhance Your
                 <br />
                 Natural Beauty
